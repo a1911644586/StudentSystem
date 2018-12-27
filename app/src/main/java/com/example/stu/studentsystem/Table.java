85718702ea5f16b4760ba9894a39d6b662fa4723
StudentSystem <www.1911644586@qq.com>
2018-12-27 09:25:31 +0800

    定义数据库名字和变量

---
+++ app/src/main/java/com/example/stu/studentsystem/Table.java
@@ -0,0 +1,15 @@
+package com.example.stu.studentsystem;
+
+public class Table {
+    public static final String STUDENT_TABLE = "student";
+    public static final class StudentColumns {
+        public static final String ID = "_id";
+        public static final String NAME = "name";
+        public static final String AGE = "age";
+        public static final String SEX = "sex";
+        public static final String PROFESSION = "profession";
+        public static final String SCORE = "score";
+        public static final String TRAIN_DATE = "train_date";
+        public static final String MODIFY_TIME = "modify_time";
+    }
+}
