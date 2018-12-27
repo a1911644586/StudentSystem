143a9e2b27f0f20fde5658feb8b4421be1cf1c18
StudentSystem <www.1911644586@qq.com>
2018-12-27 09:19:21 +0800

    修改了界面

---
+++ app/src/main/java/com/example/stu/studentsystem/StudentSystem.java
@@ -0,0 +1,13 @@
+package com.example.stu.studentsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentSystem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_system);
+    }
+}
